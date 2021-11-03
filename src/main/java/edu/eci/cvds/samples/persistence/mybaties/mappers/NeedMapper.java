50bb79999943d9344d32877cc8604afc9f23c8b8
Camilo Pichimata <camilo.pichimata@mail.escuelaing.edu.co>
2021-11-03 18:11:16 -0500

    Se agregaron nuevos métodos

--- src/main/java/edu/eci/cvds/samples/persistence/mybaties/mappers/NeedMapper.java
+++ src/main/java/edu/eci/cvds/samples/persistence/mybaties/mappers/NeedMapper.java
@@ -1,5 +1,15 @@
 package edu.eci.cvds.samples.persistence.mybaties.mappers;
 
-public interface NeedMapper {
+import java.util.List;
 
+import edu.eci.cvds.samples.entities.Need;
+
+public interface NeedMapper {
+	public List<Need> searchNeeds();
+	
+	public void registerNeed();
+	
+	public void updateStateNeed();
+	
+	public void deleteNeeds();
 }
