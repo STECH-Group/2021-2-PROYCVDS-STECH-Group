98c86d5a621e2c371a84b0114fbb241919ee1478
Jose Gamboa <jose.gamboa@mail.escuelaing.edu.co>
2021-11-03 17:36:13 -0500

    Creacion de las clases para necesidades

---
+++ src/main/java/edu/eci/cvds/samples/entities/Need.java
@@ -0,0 +1,101 @@
+package edu.eci.cvds.samples.entities;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class Need implements Serializable{
+	
+	private int id;
+	private Category category;
+	private String name;
+	private String description;
+	private int urgency;
+	private Date creationDate;
+	private String state;
+	private Date modifyDate;
+	
+	public Need(int id, Category category, String name, String description, int urgency, Date creationDate,
+			String state, Date modifyDate) {
+		super();
+		this.id = id;
+		this.category = category;
+		this.name = name;
+		this.description = description;
+		this.urgency = urgency;
+		this.creationDate = creationDate;
+		this.state = state;
+		this.modifyDate = modifyDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getUrgency() {
+		return urgency;
+	}
+
+	public void setUrgency(int urgency) {
+		this.urgency = urgency;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public Date getModifyDate() {
+		return modifyDate;
+	}
+
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Need [id=" + id + ", category=" + category + ", name=" + name + ", description=" + description
+				+ ", urgency=" + urgency + ", creationDate=" + creationDate + ", state=" + state + ", modifyDate="
+				+ modifyDate + "]";
+	}
+
+}
--- tables.sql
+++ tables.sql
@@ -1,18 +1,31 @@
-create sequence id;
-
 create table if not exists USERS( 
-	user_Name varchar(40) primary key,
+	user_name varchar(40) primary key,
 	name varchar(50) not null,
 	mail varchar(80) unique not null,
 	passwd varchar(70) not null,
 	rol varchar(20) not null
  );
+ 
+ create sequence id_Categories;
 
 create table if not exists CATEGORIES(
 	ID int primary key,
 	name varchar(50) unique not null,
 	description varchar(400) not null,
-	creation_Date Date not null,
+	creation_date Date not null,
 	state varchar(10) not null,
-	modify_Date Date not null
+	modify_date Date not null
 );
+
+create sequence id_Needs;
+
+create table if not exists NEEDS( 
+	id int primary key,
+	category varchar(50) references categories(name) not null,
+	name varchar(80) not null,
+	description varchar(400) not null,
+	urgency int not null,
+	creation_date Date not null,
+	state varchar(10) not null,
+	modify_date Date not null
+ );
