bada87c998b450cf3fc38e09634ea936a0b1d46b
Camilo Pichimata <camilo.pichimata@mail.escuelaing.edu.co>
2021-11-04 09:10:18 -0500

    Implementación métodos de la tabla necesidades

--- src/main/java/edu/eci/cvds/samples/persistence/mybaties/MyBatisNeedDAO.java
+++ src/main/java/edu/eci/cvds/samples/persistence/mybaties/MyBatisNeedDAO.java
@@ -5,9 +5,10 @@ import org.apache.ibatis.exceptions.PersistenceException;
 
 import com.google.inject.Inject;
 
+import edu.eci.cvds.samples.persistence.NeedDAO;
 import edu.eci.cvds.samples.persistence.mybaties.mappers.NeedMapper;
 
-public class MyBatisNeedDAO {
+public class MyBatisNeedDAO implements NeedDAO{
 
 	@Inject
 	private NeedMapper needMapper;
@@ -15,9 +16,28 @@ public class MyBatisNeedDAO {
 	public void registrarNecesidad(String category, String name, String description, int urgency, String state) throws PersistenceException {
 		try {	
 			needMapper.registerNeed(category, name, description, urgency, state);
-			}
-			catch(org.apache.ibatis.exceptions.PersistenceException e) {
-	            throw new PersistenceException("Error al crear la categoria, el nombre ya esta en uso",e);
-			}
+		} catch(org.apache.ibatis.exceptions.PersistenceException e) {
+	        throw new PersistenceException("Error al registrar la necesidad",e);
+		}
+	}
+	
+	private void actualizarEstadoNecesidad(int id, String state) throws PersistenceException{
+		try {	
+			needMapper.updateStateNeed(id, state);
+		} catch(org.apache.ibatis.exceptions.PersistenceException e) {
+	        throw new PersistenceException("Error al actualizar la necesidad",e);
+		}
+	}
+	
+	private void eliminarNecesidadPorId(int id) throws PersistenceException{
+		try {	
+			needMapper.deleteNeedById(id);
+		} catch(org.apache.ibatis.exceptions.PersistenceException e) {
+	        throw new PersistenceException("La necesidad que desea eliminar no existe",e);
+		}
+	}
+	
+	private void eliminarNecesidades() {	
+		needMapper.deleteNeeds();
 	}
 }
