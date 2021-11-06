439a3a22504f964fe6c862f96bd795d1397c7d99
Camilo Pichimata <camilo.pichimata@mail.escuelaing.edu.co>
2021-11-06 10:37:08 -0500

    Se agregaron los métodos correspondientes al manejo de necesidades

--- src/main/java/edu/eci/cvds/samples/persistence/NeedDAO.java
+++ src/main/java/edu/eci/cvds/samples/persistence/NeedDAO.java
@@ -5,5 +5,11 @@ import org.apache.ibatis.exceptions.PersistenceException;
 public interface NeedDAO {
 	
 	public void registrarNecesidad(String category,  String description, int urgency, String state) throws PersistenceException;
+	
+	public void actualizarEstadoNecesidad(int id, String state) throws PersistenceException;
+	
+	public void eliminarNecesidadPorId(int id) throws PersistenceException;
+	
+	public void eliminarNecesidades();
 
 }
--- src/main/java/edu/eci/cvds/samples/persistence/mybaties/MyBatisNeedDAO.java
+++ src/main/java/edu/eci/cvds/samples/persistence/mybaties/MyBatisNeedDAO.java
@@ -14,33 +14,36 @@ public class MyBatisNeedDAO implements NeedDAO{
 	@Inject
 	private NeedMapper needMapper;
 	
+	@Override
 	public void registrarNecesidad(String category,  String description, int urgency, String state) throws PersistenceException {
 		try {	
 			Subject user = SecurityUtils.getSubject();
 			needMapper.registerNeed(category, user.getSession().getAttribute("name").toString(), description, urgency, state);
-			}
-			catch(org.apache.ibatis.exceptions.PersistenceException e) {
-	            throw new PersistenceException("Error al crear la categoria, el nombre ya esta en uso",e);
-			}
+		} catch(PersistenceException e) {
+	        throw new PersistenceException("Error al crear la categoria, el nombre ya esta en uso",e);
+		}
 	}
 	
-	private void actualizarEstadoNecesidad(int id, String state) throws PersistenceException{
+	@Override
+	public void actualizarEstadoNecesidad(int id, String state) throws PersistenceException{
 		try {	
 			needMapper.updateStateNeed(id, state);
-		} catch(org.apache.ibatis.exceptions.PersistenceException e) {
+		} catch(PersistenceException e) {
 	        throw new PersistenceException("Error al actualizar la necesidad",e);
 		}
 	}
 	
-	private void eliminarNecesidadPorId(int id) throws PersistenceException{
+	@Override
+	public void eliminarNecesidadPorId(int id) throws PersistenceException{
 		try {	
 			needMapper.deleteNeedById(id);
-		} catch(org.apache.ibatis.exceptions.PersistenceException e) {
+		} catch(PersistenceException e) {
 	        throw new PersistenceException("La necesidad que desea eliminar no existe",e);
 		}
 	}
 	
-	private void eliminarNecesidades() {	
+	@Override
+	public void eliminarNecesidades() {	
 		needMapper.deleteNeeds();
 	}
 }
